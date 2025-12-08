0b5e6e797d5f6242e4eaa4cd1e3918f4f98eff56
Ph3nixe09 <saulnier.etienne09@gmail.com>
2025-12-07 21:27:02 -0500

    Fonctionne mais seulement d'un côté

--- AA/AA_scenes/nameScene.py
+++ AA/AA_scenes/nameScene.py
@@ -7,6 +7,8 @@ from AA.AA_scenes.sceneClass import Scene
 from AA.AA_utils import inputManager, musicManager, settings
 from AA.AA_utils.fontManager import upheaval
 
+#Test de single VS multiplayer
+singleplayer = False
 
 class NameScene(Scene):
 
@@ -37,14 +39,43 @@ class NameScene(Scene):
                     icon_dir, "Quitter - Select.png")).convert_alpha(),
             "joystick":
                 pygame.image.load(os.path.join(
-                    icon_dir, "Déplacer - Joystick.png")).convert_alpha()
+                    icon_dir, "Déplacer - Joystick.png")).convert_alpha(),
         }
         # Scale input icons uniformly
         for key, img in list(self._icons.items()):
             self._icons[key] = pygame.transform.scale(img, (150, 50))
 
+        self.img_ver = {
+            "check":
+        pygame.image.load(os.path.join(
+            images_dir, "check.png")).convert_alpha(),
+            "x":
+        pygame.image.load(os.path.join(
+            images_dir, "x.png")).convert_alpha()}
+
+        self.lettres = pygame.Surface((settings.WINDOW_SIZE[0]//2,settings.WINDOW_SIZE[1]), pygame.SRCALPHA)
+        x = 73
+        y = 150
+        lap = 0
+        for i in alpha:
+            text = upheaval(i, 75, (255, 255, 255))
+            if i == "M" or i == "W":
+                self.lettres.blit(text, (x-4, y))
+            elif i == "Q":
+                self.lettres.blit(text, (x, y-3))
+            elif i == "Y":
+                self.lettres.blit(text, (x-2, y))
+            else:
+                self.lettres.blit(text, (x, y))
+            x += 80
+            lap += 1
+            if lap%5 == 0:
+                y += 70
+                x = 73
+
         self.selection = pygame.image.load(os.path.join(images_dir, "selection.png")).convert_alpha()
-        self.selection_pos = [56,150]
+        self.selection_pos1 = [56,150]
+        self.selection_pos2 = [567,150]
 
         self.alphabet = [
             ["A","B","C","D","E"],
@@ -57,6 +88,13 @@ class NameScene(Scene):
         self.y = 0
         self.x = 0
         self.nom = []
+        self.ready = False
+
+        #2nd player
+        self.y2 = 0
+        self.x2 = 0
+        self.nom2 = []
+        self.ready2 = False
 
     def initScene(self):
         super().initScene()
@@ -68,77 +106,108 @@ class NameScene(Scene):
             new_axes = self._inputManager.getAxesActive(i,
                                                         onlyCheckForNew=True)
             if inputManager.AxisInputs.Y_UP in new_axes:
-                if self.y > 0:
-                    if self.y == 5:
-                        self.x = 0
-                    self.y -= 1
-                    self.selection_pos[1] -= 70
+                if i == 0:
+                    if self.y > 0:
+                        if self.y == 5:
+                            self.x = 0
+                        self.y -= 1
+                        self.selection_pos1[1] -= 70
+                else:
+                    if self.y2 > 0:
+                        if self.y2 == 5:
+                            self.x2 = 0
+                        self.y2 -= 1
+                        self.selection_pos2[1] -= 70
                 print("UP")
             elif inputManager.AxisInputs.Y_DOWN in new_axes:
-                if self.y <= 4:
-                    self.y += 1
-                    if self.y == 5:
-                        self.x = 0
-                        self.selection_pos[0] = 56
-                    self.selection_pos[1] += 70
+                if i == 0:
+                    if self.y <= 4:
+                        self.y += 1
+                        if self.y == 5:
+                            self.x = 0
+                            self.selection_pos1[0] = 56
+                        self.selection_pos1[1] += 70
+                else:
+                    if self.y2 <= 4:
+                        self.y2 += 1
+                        if self.y2 == 5:
+                            self.x2 = 0
+                            self.selection_pos2[0] = 56
+                        self.selection_pos2[1] += 70
                 print("DOWN")
             elif inputManager.AxisInputs.X_LEFT in new_axes:
-                if self.x > 0:
-                    if not self.y == 5:
-                        self.x -= 1
-                        self.selection_pos[0] -= 80
+                if i == 0:
+                    if self.x > 0:
+                        if not self.y == 5:
+                            self.x -= 1
+                            self.selection_pos1[0] -= 80
+                    else:
+                        if self.x2 > 0:
+                            if not self.y2 == 5:
+                                self.x2 -= 1
+                                self.selection_pos2[0] -= 80
                 print("LEFT")
             elif inputManager.AxisInputs.X_RIGHT in new_axes:
-                if self.x <= 3:
-                    if not self.y == 5:
-                        self.x += 1
-                        self.selection_pos[0] += 80
+                if i == 0:
+                    if self.x <= 3:
+                        if not self.y == 5:
+                            self.x += 1
+                            self.selection_pos1[0] += 80
+                    else:
+                        if self.x2 <= 3:
+                            if not self.y2 == 5:
+                                self.x2 += 1
+                                self.selection_pos2[0] += 80
                 print("RIGHT")
 
             new_btns = self._inputManager.getBtnsPressed(i,
                                                          onlyCheckForNew=True)
             if inputManager.ButtonInputs.A in new_btns:
                 # Trigger action
-                if len(self.nom) < 3:
-                    self.nom.append(self.alphabet[self.y][self.x])
+                if i == 0:
+                    if len(self.nom) < 3:
+                        self.nom.append(self.alphabet[self.y][self.x])
+                    else:
+                        if len(self.nom2) < 3:
+                            self.nom2.append(self.alphabet[self.y][self.x])
                 print(f"A, {self.nom}")
 
             elif inputManager.ButtonInputs.B in new_btns:
                 # Trigger action
-                if len(self.nom) > 0:
-                    self.nom.pop()
+                if i == 0:
+                    if len(self.nom) > 0:
+                        self.nom.pop()
+                    self.ready = False
+                else:
+                    if len(self.nom2) > 0:
+                        self.nom2.pop()
+                    self.ready2 = False
                 print(f"B, {self.nom}")
+
+            elif inputManager.ButtonInputs.START in new_btns:
+                if i == 0:
+                    if not self.nom == ["C","P","U"] and len(self.nom) == 3:
+                        print(f"{self.nom[0]} {self.nom[1]} {self.nom[2]}")
+                        self.ready = True
+                    else:
+                        print("ERREUR")
+                else:
+                    if not self.nom == ["C","P","U"] and len(self.nom) == 3:
+                        print(f"{self.nom[0]} {self.nom[1]} {self.nom[2]}")
+                        self.ready = True
+                    else:
+                        print("ERREUR")
 
         self._mainApp.blit(
             self.bg_image,
             self.bg_image.get_rect(center=self._mainApp.get_rect().center))
 
-        screen.blit(self.selection, (self.selection_pos[0],self.selection_pos[1]))
+        screen.blit(self.selection, (self.selection_pos1[0],self.selection_pos1[1]))
+        screen.blit(self.lettres, (0,0))
 
-        x = 73
-        y = 150
-        lap = 0
-        for n in range(2):
-            for i in alpha:
-                text = upheaval(i, 75, (255, 255, 255))
-                if i == "M" or i == "W":
-                    screen.blit(text, (x-4, y))
-                elif i == "Q":
-                    screen.blit(text, (x, y-3))
-                elif i == "Y":
-                    screen.blit(text, (x-2, y))
-                else:
-                    screen.blit(text, (x, y))
-                x += 80
-                lap += 1
-                if lap%5 == 0:
-                    y += 70
-                    x = 73
-                    if n == 1:
-                        x = 585
-            x = 585
-            lap = 0
-            y = 150
+        if not singleplayer:
+            screen.blit(self.selection, (self.selection_pos2[0],self.selection_pos2[1]))
+            screen.blit(self.lettres, (settings.WINDOW_SIZE[0]//2, 0))
 
         # Button input instructions at bottom
         # B input
@@ -153,6 +222,11 @@ class NameScene(Scene):
         self._mainApp.blit(self._icons["select"],
                            (15, settings.WINDOW_SIZE[1] - 60))
 
+        if self.ready:
+            self._mainApp.blit(self.img_ver["check"],
+                               (15, 60))
+            self._mainApp.blit(self.img_ver["x"],
+                               (600, 60))
 
         # Call parent loop to handle input and quitting
         return super().loopScene(events)
