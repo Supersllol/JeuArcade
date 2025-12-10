e6b15dd04ef5abf0ae35aad7d7602ceb838131db
Supersllol <simon_lacroix@outlook.com>
2025-12-10 18:46:06 -0500

    change pc fps

--- AA/AA_utils/settings.py
+++ AA/AA_utils/settings.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import os
 
-FRAMERATE = 15
+FRAMERATE = 60
 WINDOW_SIZE = (1024, 768)
 
 PARENT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
